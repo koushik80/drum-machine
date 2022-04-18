d95de6ffebffff80e8f07bbc5972932bd21ceab3
Koushik Dey <koushik.dey@live.com>
2022-04-19 02:27:17 +0300

    hap

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
-    <App />, document.querySelector("#root")
+    <App />
   </React.StrictMode>
 )
 ;
