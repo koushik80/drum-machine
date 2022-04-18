91dd41d9e94c592469c7b8a55865f341f6e37c1d
Koushik Dey <koushik.dey@live.com>
2022-04-18 23:19:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center"><a href="####" target="_blank">Markdown Previewer</a></div>
+<h1 align="center">fcc_Frontend Dev Project 3</h1><br>
+<h3 align="left">Technologies:</h3>
+<ul>
+<li>React</li>
+<li>CSS</li>
+<li></li>
+<li></li>
+</ul>
+<h1 align="left">User Stories</h1> <br>
+
+<p>User Story 1: I should be able to see an outer container with a corresponding id="drum-machine" that contains all other elements.</p>
+<p>User Story 2: Within #drum-machine I can see an element with a corresponding id="display".</p>
+<p>User Story 3: Within #drum-machine I can see 9 clickable drum pad elements, each with a class name of drum-pad, a unique id that describes the audio clip the drum pad will be set up to trigger, and an inner text that corresponds to one of the following keys on the keyboard: Q, W, E, A, S, D, Z, X, C. The drum pads MUST be in this order.</p>
+<p>User Story 4: Within each .drum-pad, there should be an HTML5 audio element which has a src attribute pointing to an audio clip, a class name of clip, and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.).</p>
+<p>User Story 5: When I click on a .drum-pad element, the audio clip contained in its child audio element should be triggered.</p>
+<p>User Story 6: When I press the trigger key associated with each .drum-pad, the audio clip contained in its child audio element should be triggered (e.g. pressing the Q key should trigger the drum pad which contains the string Q, pressing the W key should trigger the drum pad which contains the string W, etc.).</p>
+<p>User Story 7: When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique).</p>
+
+<h3 align="center">fcc Objective: Build a CodePen.io app that is functionally similar to this: </h3><br>
+<p><a href="https://codepen.io/freeCodeCamp/full/MJyNMd" target="_blank">Example</a></p>
---
+++ src/App.js
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import logo from './logo.png';
+import './drum.css';
+import { firstSoundsGroup, secondSoundsGroup } from './data';
+
+const soundsName = {
+    heaterKit: "Heater Kit",
+    smoothPianoKit: "Smooth Piano Kit"
+  };
+
+  const soundsGroup = {
+    heaterKit: firstSoundsGroup,
+    smoothPianoKit: secondSoundsGroup
+  }
+
+  const KeyboardKey = ({ play, deactivateAudio, sound: { id, key, url, keyCode } }) => {
+    const handleKeydown = (e) => {
+      if (keyCode === e.keyCode) {
+        const audio = document.getElementById(key);
+        play(key, id);
+        deactivateAudio(audio)
+      }
+    }
+    /*eslint-disable */
+    React.useEffect(() => {
+      document.addEventListener('keydown', handleKeydown);
+    }, [])
+    /*eslint-enable */
+    return (
+      <button value="test" id={keyCode} className="drum-pad" onClick={() => play(key, id)}>
+        <audio className="clip" src={url} id={key} />
+        {key}
+      </button>
+    );
+  }
+
+const Keyboard = ({ sounds, play, power, deactivateAudio }) => (
+    <div className="keyboard">
+      {power
+        ? sounds.map((sound) => <KeyboardKey sound={sound} play={play} deactivateAudio={deactivateAudio} />)
+        : sounds.map((sound) => <KeyboardKey sound={{ ...sound, url: "#" }} play={play} deactivateAudio={deactivateAudio} />)
+      }
+    </div>
+);
+  const DrumControl = ({ stop, name, power, volume, handleVolumeChange, changeSoundGroup }) => (
+    <div className="control">
+      <button onClick={stop}>Turn Power {power ? 'OFF' : 'ON'}</button>
+      <h2>Volume: %{Math.round(volume * 100)}</h2>
+      <input
+        max="1"
+        min="0"
+        step='0.01'
+        type="range"
+        value={volume}
+        onChange={handleVolumeChange}
+      />
+      <h2 id="display" >{name}</h2>
+      <button onClick={changeSoundGroup}>Change Sounds Group</button>
+    </div>
+  );
+
+const App = () => {
+  const [power, setPower] = React.useState(true);
+  const [volume, setVolume] = React.useState(1);
+  const [soundName, setSoundName] = React.useState("");
+  const [soundType, setSoundType] = React.useState("heaterKit");
+  const [sounds, setSounds] = React.useState(soundsGroup[soundType]);
+  const styleActiveKey = (key) => {
+    key.parentElement.style.backgroundColor = "#000000"
+    key.parentElement.style.color = "#ffffff"
+  }
+  const deactivatedKey = (audio) => {
+    audio.parentElement.style.backgroundColor = "#ffffff"
+    audio.parentElement.style.color = "#000000"
+  }
+
+  const deactivateAudio = (audio) => {
+    setTimeout(() => {
+      audio.parentElement.style.backgroundColor = "#ffffff"
+      audio.parentElement.style.color = "#000000"
+    }, 300)
+  }
+
+  const play = (key, sound) => {
+    setSoundName(sound)
+    const audio = document.getElementById(key);
+    styleActiveKey(audio);
+    audio.currentTime = 0;
+    audio.play();
+    deactivateAudio(audio)
+  }
+
+  const stop = () => {
+    setPower(!power)
+  }
+
+  const changeSoundGroup = () => {
+    setSoundName("")
+    if (soundType === "heaterKit") {
+      setSoundType("smoothPianoKit");
+      setSounds(soundsGroup.smoothPianoKit);
+    } else {
+      setSoundType("heaterKit");
+      setSounds(soundsGroup.heaterKit);
+    }
+  }
+  const handleVolumeChange = e => {
+    setVolume(e.target.value)
+  }
+  const setKeyVolume = () => {
+    const audioes = sounds.map(sound => document.getElementById(sound.key));
+    audioes.forEach(audio => {
+      if (audio) {
+        audio.volume = volume;
+      }
+    })
+  }
+
+  return (
+    <div className="App">
+      <div className='logo'>
+        <div className='inner-logo'>devHuß</div>
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Front End Development <code></code> Libraries Project 3
+        </p>
+        <a
+          className="App-link"
+          href="https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-drum-machine"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Drum Machine
+        </a>
+      </header>
+      <div>
+      <div id="drum-machine">
+      {setKeyVolume()}
+      <div className="wrapper">
+        <Keyboard sounds={sounds} play={play} power={power} deactivateAudio={deactivateAudio} />
+        <DrumControl
+          stop={stop}
+          power={power}
+          volume={volume}
+          deactivatedKey={deactivatedKey}
+          name={soundName || soundsName[soundType]}
+          changeSoundGroup={changeSoundGroup}
+          handleVolumeChange={handleVolumeChange}
+        />
+      </div>
+    </div>
+      
+      
+      </div>
+    </div>
+  )
+};
+
+export default App;
+
+
